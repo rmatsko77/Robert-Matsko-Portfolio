b29927c836d1e31c4a290a6a526c387aa29041b4
Robert Matsko <robertmatsko@Roberts-MacBook-Pro-4.local>
2023-06-13 16:09:34 -0400

    initial commit basic design and content

---
+++ src/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+    return (
+        <div className='about'>
+            <p>From Harrisburg Pennsylvania and based in New York City, I am a front-end web developer and photographer. With expertise in creating user-friendly interfaces and crafting visually stunning websites, I bring ideas to life online. Through my photography, I capture unique moments and tell stories. I'm passionate about leveraging technology and creativity to deliver impactful digital experiences.
+            </p>
+        </div>
+    )
+}
+
+export default About;
--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import About from './About';
+import Projects from './Projects';
+import { Link, Route, Routes } from 'react-router-dom'
+import { BsPersonCircle, BsGithub, BsInstagram, BsLinkedin } from "react-icons/bs";
+import { MdLocationPin } from "react-icons/md";
+import { AiOutlineMinus } from "react-icons/ai";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1 className='name'>Robert Matsko</h1>
+      <h2 className='title'><BsPersonCircle className='title-icon'/>Front-End Developer / Photographer</h2>
+      <h3 className='location'><MdLocationPin className='location-icon' />Brooklyn, New York</h3>
+      <div className='socials'>
+        <a href='https://github.com/rmatsko77' target='_blank'><BsGithub /></a>
+        <a href='https://www.instagram.com/r.p.matsko/' target='_blank'><BsInstagram /></a>
+        <a href='https://www.linkedin.com/in/robert-matsko-crcst-cis-905469198/' target='_blank'><BsLinkedin /></a>
+      </div>
+      <nav>
+        <ul>
+          <li><Link className='nav' to='/about'><AiOutlineMinus id='nav-icon'/>About</Link></li>
+          <li><Link className='nav' to='/projects'><AiOutlineMinus id='nav-icon'/>Projects</Link></li>
+          <li className='nav' ><AiOutlineMinus id='nav-icon'/>Photography</li>
+          <li className='nav' ><AiOutlineMinus id='nav-icon'/>Contact</li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path='/about' element={<About />} />
+        <Route path='/projects' element={<Projects />} />
+      </Routes>
     </div>
   );
 }

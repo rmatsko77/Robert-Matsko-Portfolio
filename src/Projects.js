f395c8064fe71032325fe1a6a8db5194d7794479
Robert Matsko <robertmatsko@roberts-mbp.mynetworksettings.com>
2023-06-16 14:18:16 -0400

    second deploy fix

--- src/Projects.js
+++ src/Projects.js
@@ -9,38 +9,38 @@ function Projects() {
     exit={{ opacity: 0 }}>
       <ul className='projects-list'>
         <li className='project' id='redlite'>
-          <a className='project-title' href='https://redlite.netlify.app/' target='_blank'>RedLite</a>
+          <a className='project-title' href='https://redlite.netlify.app/' target='_blank' rel="noreferrer">RedLite</a>
           <div className='project-bottom'>
             <p className='project-description'>Reddit Browser built in React Redux</p>
-            <a href='https://github.com/rmatsko77/RedLite' target='_blank' className='repo-link'>Repo</a>
+            <a href='https://github.com/rmatsko77/RedLite' target='_blank' className='repo-link' rel="noreferrer">Repo</a>
           </div>
         </li>
         <li className='project' id='whats-the-plan'>
-          <a className='project-title' href='https://whats-the-plan.netlify.app/' target='_blank'>What's the Plan?</a>
+          <a className='project-title' href='https://whats-the-plan.netlify.app/' target='_blank' rel="noreferrer">What's the Plan?</a>
           <div className='project-bottom'>
             <p className='project-description'>Simple to-do app built in React</p>
-            <a href='https://github.com/rmatsko77/react-todo-app' target='_blank' className='repo-link'>Repo</a>
+            <a href='https://github.com/rmatsko77/react-todo-app' target='_blank' rel="noreferrer" className='repo-link'>Repo</a>
           </div>
         </li>
         <li className='project' id='beat-builder'>
-          <a className='project-title' href='https://beat-builder.surge.sh/' target='_blank'>BeatBuilder</a>
+          <a className='project-title' href='https://beat-builder.surge.sh/' target='_blank' rel="noreferrer">BeatBuilder</a>
           <div className='project-bottom'>
             <p className='project-description'>Creates playlists on users spotify profile built in React</p>
-            <a href='https://github.com/rmatsko77/BeatBuilder' target='_blank' className='repo-link'>Repo</a>
+            <a href='https://github.com/rmatsko77/BeatBuilder' target='_blank' rel="noreferrer" className='repo-link'>Repo</a>
           </div>
         </li>
         <li className='project' id='quick-keys'>
-          <a className='project-title' href='https://quickkeys.netlify.app/' target='_blank'>QuickKeys</a>
+          <a className='project-title' href='https://quickkeys.netlify.app/' target='_blank' rel="noreferrer">QuickKeys</a>
           <div className='project-bottom'>
             <p className='project-description'>Keyboard that can be implimented in other apps</p>
-            <a href='https://github.com/rmatsko77/virtual-keyboard' target='_blank' className='repo-link'>Repo</a>
+            <a href='https://github.com/rmatsko77/virtual-keyboard' target='_blank' rel="noreferrer" className='repo-link'>Repo</a>
           </div>
         </li>
         <li className='project' id='calc-hub'>
-          <a className='project-title' href='https://my-practice-react-calc.netlify.app/' target='_blank'>CalcHub</a>
+          <a className='project-title' href='https://my-practice-react-calc.netlify.app/' target='_blank' rel="noreferrer">CalcHub</a>
           <div className='project-bottom'>
             <p className='project-description'>Simple calculor built in React</p>
-            <a href='https://github.com/rmatsko77/react-calc' target='_blank' className='repo-link'>Repo</a>
+            <a href='https://github.com/rmatsko77/react-calc' target='_blank' rel="noreferrer" className='repo-link'>Repo</a>
           </div>
         </li>
       </ul>
